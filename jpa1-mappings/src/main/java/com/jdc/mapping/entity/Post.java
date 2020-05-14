7c32120ab782ebcab0e5e2c21f74ff2b3fa89415
minlwin <lwin.zawmin@gmail.com>
2020-05-14 19:49:59 +0630

    Remove Imports for Warnings

--- jpa1-mappings/src/main/java/com/jdc/mapping/entity/Post.java
+++ jpa1-mappings/src/main/java/com/jdc/mapping/entity/Post.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
