7a7397a00958555c9a5e9f2a8e9e713da792cdb9
minlwin <lwin.zawmin@gmail.com>
2020-06-11 23:08:41 +0630

    Add Web Configuration for job search

---
+++ job-search/src/main/java/com/jdc/job/WebAppConfig.java
@@ -0,0 +1,16 @@
+package com.jdc.job;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebAppConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+	}
+}
--- job-search/src/main/java/com/jdc/job/model/entity/JobApplicationMessage.java
+++ job-search/src/main/java/com/jdc/job/model/entity/JobApplicationMessage.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
+@Entity
 @Data
 public class JobApplicationMessage implements Serializable {
 
