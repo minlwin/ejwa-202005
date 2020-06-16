430e17c540d784aa5d9c9e0362db3ee33ded2a25
minlwin <lwin.zawmin@gmail.com>
2020-06-16 18:31:18 +0630

    Update Address Search

--- job-search/src/main/java/com/jdc/job/api/AddressApi.java
+++ job-search/src/main/java/com/jdc/job/api/AddressApi.java
@@ -1,7 +1,11 @@
 package com.jdc.job.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jdc.job.model.BaseApi;
@@ -19,6 +23,14 @@ public class AddressApi extends BaseApi<Address, Integer>{
 	@Override
 	protected BaseService<Address, Integer> service() {
 		return service;
+	}
+	
+	@GetMapping("search")
+	public List<Address> search(
+			@RequestParam(defaultValue = "0") int division, 
+			@RequestParam(defaultValue = "0") int township, 
+			@RequestParam(defaultValue = "") String address) {
+		return service.search(division, township, address);
 	}
 
 }
--- job-search/src/main/java/com/jdc/job/model/service/AddressService.java
+++ job-search/src/main/java/com/jdc/job/model/service/AddressService.java
@@ -1,7 +1,12 @@
 package com.jdc.job.model.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.jdc.job.model.BaseRepository;
 import com.jdc.job.model.BaseService;
@@ -17,6 +22,32 @@ public class AddressService extends BaseService<Address, Integer>{
 	@Override
 	protected BaseRepository<Address, Integer> repo() {
 		return repo;
+	}
+
+	public List<Address> search(int division, int township, String address) {
+		
+		StringBuffer sb = new StringBuffer("select a from Address a where 1 = 1");
+		Map<String, Object> params = new HashMap<>();
+		
+		if(township != 0) {
+			
+			sb.append(" and a.township.id = :township");
+			params.put("township", township);
+			
+		} else {
+			if(division != 0) {
+				sb.append(" and a.township.division.id = :division");
+				params.put("division", division);
+			}
+		}
+		
+		if(!address.isEmpty()) {
+			
+			sb.append(" and (lower(a.street) like lower(:address) or lower(a.address) like lower(:address))");
+			params.put("address", "%".concat(address).concat("%"));
+		}
+  		
+		return repo.search(sb.toString(), params);
 	}
 
 }
